9a5edb97277b8571ce219216ad2ffa627288fb37
Jaison Joshy <49711083+coderjaiz@users.noreply.github.com>
2019-04-17 21:04:12 +0530

    Update FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,10 +1,10 @@
 for(let n=1;n<=100;n++)
 	{
-	if(n%3==0&&n%5==0)
+	if(n%3==0&&n%5==0) //if divisible by both 3 and 5
 		console.log("FizzBuzz");
-	else if(n%3==0)
+	else if(n%3==0) //if divisible by 3
 		console.log("Fizz");
-	else if(n%5==0)
+	else if(n%5==0) //if divisible by 5
 		console.log("Buzz");
 	else
 		console.log(n);
